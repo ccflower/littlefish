958c2b4a8f8f26a19a2ef04874de210e1ab28d7c
Yuecui Li <liyuecui@cn.ibm.com>
2018-04-20 16:22:59 +0800

    add product list

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<li>{{ todo.text }}</li>'
-});
-
 var app = new Vue({
     el: '#app',
     data: {
-        groceryList: [
-            { id: 0, text: 'Vegetables' },
-            { id: 1, text: 'Cheese' },
-            { id: 2, text: 'Whatever else humans are supposed to eat' }
-        ]
+        productList: []
+    },
+    mounted: function() {
+        Bmob.initialize("55432a69e0efff6ec4a8ade94751a978", "94342d6f5c024f58c754618ecf25444b");
+        // get data
+        let XianYuItem = Bmob.Object.extend("XianYuItem");
+        let query = new Bmob.Query(XianYuItem);
+        query.find({
+            success: function(results) {
+              console.log("共查询到 " + results.length + " 条记录");
+              // 循环处理查询到的数据
+              for (let i = 0; i < results.length; i++) {
+                let object = results[i];
+                console.log(object.id + ' - ' + object.get('name'));
+                let item = {
+                    id: object.get('id'),
+                    name: object.get('name'),
+                    price: object.get('price'),
+                    sellCount: object.get('sellCount'),
+                    panLink: object.get('panLink'),
+                    isUp: object.get('isUp'),
+                    content: object.get('content')
+                }
+                app.productList.push(item);
+              }
+            },
+            error: function(error) {
+              console.log("查询失败: " + error.code + " " + error.message);
+            }
+        });
     }
 });
