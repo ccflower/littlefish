e7fe51dd9587c9b8bd447ae4398f1892314ce1a2
Yuecui Li <liyuecui@cn.ibm.com>
2018-04-25 18:34:03 +0800

    add num feature

--- index.js
+++ index.js
@@ -4,6 +4,32 @@ var app = new Vue({
         productList: []
     },
     methods: {
+        addProductNum(product) {
+            let XianYuItem = Bmob.Object.extend("XianYuItem");
+            let query = new Bmob.Query(XianYuItem);
+            query.get(product.id, {
+                success: function(xianYuItem) {
+                    let addConfirm = confirm("确定数量 + 1 吗？");
+                    if (addConfirm) {
+                        console.log(product);
+                        xianYuItem.fetchWhenSave(true);
+                        xianYuItem.set('sellCount', product.sellCount + 1);
+                        xianYuItem.save();
+                        product.sellCount = product.sellCount + 1;
+                        for (let i = 0; i < app.productList.length; i++) {
+                            if (app.productList[i].id == product.id) {
+                                Vue.set(app.productList, i, product);
+                            }
+                        }
+                    } else {
+                        console.log("取消增加数量...");
+                    }
+                },
+                error: function(object, error) {
+                    console.log(error);
+                }
+            });            
+        },
         generateDeliveryContent(product) {
             let clipBoardContent = "====================\n" +
             "亲~收货了~\n" +
@@ -18,7 +44,7 @@ var app = new Vue({
         generateDeployContent(product) {
             let clipBoardContent = product.name +
              "，内容见截图，全套资料低价打包带走，请勿议价，拍下发百度网盘链接。PS:网络电子资料，一旦售出概不退货，拍前请留意谢谢。";
-            
+
             this.copytoClipboard(clipBoardContent);
         },
         copytoClipboard(text) {
@@ -28,35 +54,39 @@ var app = new Vue({
             document.execCommand("Copy"); // 执行浏览器复制命令
             alert("已复制好，可贴粘。");
             template.innerHTML = "";
+        },
+        getXianYuList() {
+            let XianYuItem = Bmob.Object.extend("XianYuItem");
+            let query = new Bmob.Query(XianYuItem);
+            query.find({
+                success: function(results) {
+                //console.log("共查询到 " + results.length + " 条记录");
+                // 循环处理查询到的数据
+                for (let i = 0; i < results.length; i++) {
+                    let object = results[i];
+                    //console.log(object.id + ' - ' + object.get('name'));
+                    let item = {
+                        id: object.id,
+                        name: object.get('name'),
+                        price: object.get('price'),
+                        sellCount: object.get('sellCount'),
+                        panLink: object.get('panLink'),
+                        isUp: object.get('isUp'),
+                        content: object.get('content')
+                    }
+                    app.productList.push(item);
+                    console.log(app.productList[i].id);
+                }
+                },
+                error: function(error) {
+                console.log("查询失败: " + error.code + " " + error.message);
+                }
+            });
         }
     },
     mounted: function() {
         Bmob.initialize("55432a69e0efff6ec4a8ade94751a978", "94342d6f5c024f58c754618ecf25444b");
-        // get data
-        let XianYuItem = Bmob.Object.extend("XianYuItem");
-        let query = new Bmob.Query(XianYuItem);
-        query.find({
-            success: function(results) {
-              console.log("共查询到 " + results.length + " 条记录");
-              // 循环处理查询到的数据
-              for (let i = 0; i < results.length; i++) {
-                let object = results[i];
-                console.log(object.id + ' - ' + object.get('name'));
-                let item = {
-                    id: object.get('id'),
-                    name: object.get('name'),
-                    price: object.get('price'),
-                    sellCount: object.get('sellCount'),
-                    panLink: object.get('panLink'),
-                    isUp: object.get('isUp'),
-                    content: object.get('content')
-                }
-                app.productList.push(item);
-              }
-            },
-            error: function(error) {
-              console.log("查询失败: " + error.code + " " + error.message);
-            }
-        });
+        this.getXianYuList();
+        
     }
 });
